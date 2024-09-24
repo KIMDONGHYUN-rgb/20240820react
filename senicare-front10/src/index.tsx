ffa0d35cc2b37be03fdce76fedbc4b8abb4f6d45
KIMDONGHYUN-rgb <s010314@naver.com>
2024-09-24 17:44:20 +0900

    2024-09-24 17:44

--- senicare-front10/src/index.tsx
+++ senicare-front10/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Senicare from './Senicare';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Senicare />
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ senicare-front10/src/types/index.ts
@@ -0,0 +1,7 @@
+import Tool from "./tool.interface";
+import SignInUser from "./sign-in-user.interface";
+
+export type {
+    Tool,
+    SignInUser
+}
